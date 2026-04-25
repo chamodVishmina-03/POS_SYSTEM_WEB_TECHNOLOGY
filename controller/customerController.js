c6d4f44247430d7b767f9e2ea39a2ac9890a1801
chamodvishmina <chamodvishmina4@gmail.com>
2026-04-25 23:25:43 +0530

    create customer load table

--- controller/customerController.js
+++ controller/customerController.js
@@ -20,5 +20,23 @@ const generateCustomerId = () => {
 
 
 
+//================================== load table =================================
+const loadCustomerTable = () => {
+    const tbody = $("#customer_tbody");
+    tbody.empty();
+
+    const customers = getCustomerdata();
+    customers.forEach((c, index) => {
+        const row = `
+            <tr data-index="${index}">
+                <td>${c.id}</td>
+                <td>${c.getName()}</td>
+                <td>${c.getContract()}</td>
+                <td>${c.getAddress()}</td>
+            </tr>`;
+        tbody.append(row);
+    });
+};
+
 
 
