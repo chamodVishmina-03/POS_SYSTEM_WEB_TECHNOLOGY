c51d547344afbe0db4ccb59764802afbdc36e688
chamodvishmina <chamodvishmina4@gmail.com>
2026-04-27 16:36:55 +0530

    create item constructor

--- model/itemModel.js
+++ model/itemModel.js
@@ -8,4 +8,13 @@ class Item {
     #qty;
     #category_id;
 
+    constructor(item_id,item_name,unit_price,qty,category_id) {
+        this.#item_id = item_id;
+        this.#item_name = item_name;
+        this.#unit_price = unit_price;
+        this.#category_id = category_id;
+        this.#qty = qty;
+        this.#category_id = category_id;
+    }
+
 }
