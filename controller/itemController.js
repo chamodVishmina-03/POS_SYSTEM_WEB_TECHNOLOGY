d03660d343d19d2a63cb13d91237a694f832525f
chamodvishmina <chamodvishmina4@gmail.com>
2026-04-27 17:26:03 +0530

    create category dopdown , load item tbl

--- controller/itemController.js
+++ controller/itemController.js
@@ -8,5 +8,70 @@ import {addItem,
 } from "../model/itemModel.js";
 
 import {getCategorydata} from "../model/categoryModel.js";
+import {getCustomerdata} from "../model/customerModel";
+
+
+
+
+
+// ========================= generate item ID =========================
+
+
+const genarateItemId = () => {
+    const itemIds = getItemDataById("itemId");
+    if (items.length === 0) return "I001";
+    const lastId = items[items.length - 1].id;
+    const num = parseInt(lastId.replace("I", "")) + 1;
+    return "I" + String(num).padStart(3, "0");
+
+};
+
+
+
+// =========================Category dropdown =========================
+
+const loadCategoryDropdown = () => {
+
+            $('#categoryItemId_input').empty();
+
+            $('#categoryItemId_input').append('<option value="">-- Select Category --</option>');
+
+            getCategorydata().map(cat => {
+
+                $('#categoryItemId_input').append(`<option value="${cat.id}">
+                                                                  ${cat.id} - ${cat.getName()
+                                                                   }</option>`);
+            });
+};
+
+
+
+
+
+
+// =========================Load Item table =========================
+
+const loadItemTbl = () => {
+
+
+    $('#item_tbody').empty();
+
+    let item_db = getCustomerdata();
+
+    item_db.map((item, index) => {
+        let new_row = `<tr data-index="${index}">
+                            <td>${item.id}</td>
+                            <td>${item.getName()}</td>
+                            <td>${item.getUnitPrice()}</td>
+                            <td>${item.getQty()}</td>
+                            <td>${item.getCategoryId()}</td>
+                       </tr>`;
+        $('#item_tbody').append(new_row);
+    });
+
+}
+
+
+
 
 
