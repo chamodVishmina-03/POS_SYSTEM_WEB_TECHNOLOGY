2a44427da7f938bf1ec99965945e9b43da052802
chamodvishmina <chamodvishmina4@gmail.com>
2026-04-27 09:09:03 +0530

    create category reset form

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -10,8 +10,12 @@ import {
 
 // =========================generate id =========================
 const generateCategoryId = () => {
+
+
     const categories = getCategorydata();
+
     if (categories.length === 0) return "CAT001";
+
     const lastId = categories[categories.length - 1].id;
     const num = parseInt(lastId.replace("CAT", "")) + 1;
     return "CAT" + String(num).padStart(3, "0");
@@ -20,16 +24,36 @@ const generateCategoryId = () => {
 
 // =========================Table load =========================
 const loadCategoryTable = () => {
+
     const tbody = $("#category_tbody");
     tbody.empty();
 
+
     const categories = getCategorydata();
     categories.forEach((c, index) => {
+
         const row = `
+
             <tr data-index="${index}">
                 <td>${c.id}</td>
                 <td>${c.getName()}</td>
             </tr>`;
+
         tbody.append(row);
+
+
     });
+};
+
+
+
+
+
+// ================== reset forms =========================
+const resetForm = () => {
+
+
+    $("#categoryId_input").val(generateCategoryId());
+    $("#categoryName_input").val("");
+
 };
