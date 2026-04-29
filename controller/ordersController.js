b62e3360efcf6737515927bb64faf2ea80a6764e
chamodvishmina <chamodvishmina4@gmail.com>
2026-04-29 20:00:57 +0530

    import item , customer,order

--- controller/ordersController.js
+++ controller/ordersController.js
@@ -1,4 +1,8 @@
 import {addOrder,getAllOrders,getOrderById} from "../model/ordersModel.js";
+import {getCustomerdata} from "../model/customerModel.js";
+import {item_id}from "../model/itemModel.js";
+
+
 
 
 
