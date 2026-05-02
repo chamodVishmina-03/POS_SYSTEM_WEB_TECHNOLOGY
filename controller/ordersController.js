a9fa9a5459f133ee9ee6913e327a472c56b548f9
chamodvishmina <chamodvishmina4@gmail.com>
2026-05-02 09:09:04 +0530

    generate order id

--- controller/ordersController.js
+++ controller/ordersController.js
@@ -4,3 +4,11 @@ import {getItemdata, getItemDataById} from '../model/itemModel.js';
 import {currentOrderItems} from "../db/db.js";
 
 
+// ========================= generate    Order id =========================
+const generateOrderId = () => {
+    const orders = getAllOrders();
+    if (orders.length === 0) return "ORD001";
+    const lastId = orders[orders.length - 1].order_id;
+    const num = parseInt(lastId.replace("ORD", "")) + 1;
+    return "ORD" + String(num).padStart(3, "0");
+};
